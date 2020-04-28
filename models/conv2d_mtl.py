26e2eb49808b1f5b262dd5b7b20f296ba5ba4a50
Gursharan Ahir <42671059+ahirsharan@users.noreply.github.com>
2020-04-29 03:04:00 +0530

    Update conv2d_mtl.py

--- models/conv2d_mtl.py
+++ models/conv2d_mtl.py
@@ -1,13 +1,4 @@
-##+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-## Created by: Yaoyao Liu
-## Modified from: https://github.com/pytorch/pytorch
-## Tianjin University
-## liuyaoyao@tju.edu.cn
-## Copyright (c) 2019
-##
-## This source code is licensed under the MIT-style license found in the
-## LICENSE file in the root directory of this source tree
-##+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 """ MTL CONV layers. """
 import math
 import torch
