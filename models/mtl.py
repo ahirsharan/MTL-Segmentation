76011154b263b82623e398bbc7fd97dc760d1e09
Gursharan Ahir <42671059+ahirsharan@users.noreply.github.com>
2020-04-29 03:03:43 +0530

    Update mtl.py

--- models/mtl.py
+++ models/mtl.py
@@ -1,12 +1,4 @@
-##+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-## Created by: Yaoyao Liu
-## Tianjin University
-## liuyaoyao@tju.edu.cn
-## Copyright (c) 2019
-##
-## This source code is licensed under the MIT-style license found in the
-## LICENSE file in the root directory of this source tree
-##+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 """ Model for meta-transfer learning. """
 import  torch
 import torch.nn as nn
