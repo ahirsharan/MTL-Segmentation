5c4846ae395fdf16caf74bcdb6d7e994203077fe
Gursharan Ahir <ahir.sharan@gmail.com>
2020-05-10 17:47:26 +0530

    Update losses.py

--- utils/losses.py
+++ utils/losses.py
@@ -6,9 +6,9 @@ from utils.lovasz_losses import lovasz_softmax
 
 def make_one_hot(labels, classes):
     one_hot = torch.FloatTensor(labels.size()[0], classes, labels.size()[2], labels.size()[3]).zero_()
-    target = one_hot.scatter_(1, labels.data, 1)
     if(torch.cuda.is_available()):
-        target=target.cuda
+        one_hot=one_hot.cuda()
+    target = one_hot.scatter_(1, labels.data, 1)
     return target
 
 def get_weights(target):
