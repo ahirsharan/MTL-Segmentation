3b801d8f693a6c0020c2fb3cf469e0107d1541fe
Gursharan Ahir <ahir.sharan@gmail.com>
2020-05-10 16:24:20 +0530

    Update run_meta.py

--- run_meta.py
+++ run_meta.py
@@ -3,7 +3,7 @@
 """ Generate commands for meta-train phase. """
 import os
 
-def run_exp(num_batch=100, shot=1, query=15, lr1=0.0001, lr2=0.001, base_lr=0.01, update_step=10, gamma=0.5):
+def run_exp(num_batch=100, shot=1, query=2, lr1=0.0005, lr2=0.005, base_lr=0.01, update_step=10, gamma=0.5):
     max_epoch = 2
     step_size = 10
     way=5
@@ -25,5 +25,5 @@ def run_exp(num_batch=100, shot=1, query=15, lr1=0.0001, lr2=0.001, base_lr=0.01
     os.system(the_command + ' --phase=meta_train')
     os.system(the_command + ' --phase=meta_eval')
 
-run_exp(num_batch=50, shot=1, query=15, lr1=0.0001, lr2=0.001, base_lr=0.01, update_step=100, gamma=0.5)
+run_exp(num_batch=50, shot=1, query=2, lr1=0.0005, lr2=0.005, base_lr=0.01, update_step=10, gamma=0.5)
 #run_exp(num_batch=100, shot=5, query=15, lr1=0.0001, lr2=0.001, base_lr=0.01, update_step=100, gamma=0.5)
