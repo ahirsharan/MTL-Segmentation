f13011e644d6aded17f3dfb1270958c5a57023b5
Gursharan Ahir <ahir.sharan@gmail.com>
2020-05-12 18:33:29 +0530

    Update run_pre.py

--- run_pre.py
+++ run_pre.py
@@ -11,6 +11,7 @@ def run_exp(lr=0.1, gamma=0.2, step_size=20):
     way=2
     shot =1
     base_lr = 0.05
+    pre_init_weights='./logs/pre/COCO_UNet_batchsize6_lr0.1_gamma0.2_step20_maxepoch200/epoch20.pth'
     
     the_command = 'python3 main.py' \
         + ' --pre_max_epoch=' + str(max_epoch) \
@@ -23,7 +24,8 @@ def run_exp(lr=0.1, gamma=0.2, step_size=20):
         + ' --base_lr=' + str(base_lr) \
         + ' --pre_lr=' + str(lr) \
         + ' --phase=pre_train' \
-        + ' --way=' + str(way)    
+        + ' --way=' + str(way) \
+        + ' --pre_init_weights='+pre_init_weights
 
     os.system(the_command)
 
