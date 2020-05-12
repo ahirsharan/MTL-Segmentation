3c8802be6e9f69593f5c7725fffad58b91e7ab49
Gursharan Ahir <ahir.sharan@gmail.com>
2020-05-12 12:13:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     parser.add_argument('--mdataset_dir', type=str, default='../Datasets/Fewshot/') # Dataset folder
     parser.add_argument('--max_epoch', type=int, default=200) # Epoch number for meta-train phase
     parser.add_argument('--num_batch', type=int, default=100) # The number for different tasks used for meta-train
-    parser.add_argument('--num_classes', type=int, default=10)# Total number of pre-labelled classes 
+    parser.add_argument('--num_classes', type=int, default=6)# Total number of pre-labelled classes 
     parser.add_argument('--way', type=int, default=2) # Way number, how many classes in a task
     parser.add_argument('--shot', type=int, default=1) # Shot number, how many samples for one class in a task
     parser.add_argument('--train_query', type=int, default=1) # The number of meta train samples for each class in a task
