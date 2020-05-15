729fc13dbdad5c8837a4a60b34291689e1e0a87e
Gursharan Ahir <ahir.sharan@gmail.com>
2020-05-15 13:37:22 +0530

    Update mdataset_loader.py

--- dataloader/mdataset_loader.py
+++ dataloader/mdataset_loader.py
@@ -45,7 +45,10 @@ class mDatasetLoader(Dataset):
             this_folder_images = os.listdir(imf)
             for im in this_folder_images:
                 data.append(osp.join(imf, im))
-                label.append(osp.join(lbf, im[:-3]+'png'))    
+                if(im[-4]=='.'):
+                    label.append(osp.join(lbf, im[:-3]+'png'))    
+                else:
+                    label.append(osp.join(lbf, im[:-4]+'png'))
                 labeln.append(idx)
             
         # Set data, label and class number to be accessable from outside
