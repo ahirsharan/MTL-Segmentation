b62300fdf8738e76a236993da89ef30dd1f9624c
Gursharan Ahir <ahir.sharan@gmail.com>
2020-05-15 13:30:40 +0530

    Update run_meta.py

--- run_meta.py
+++ run_meta.py
@@ -7,7 +7,7 @@ def run_exp(num_batch=50, shot=3, teshot=1, query=1, lr1=0.0005, lr2=0.005, base
     max_epoch = 1
     step_size = 20
     way=2
-    gpu=0
+    gpu=1
        
     the_command = 'python3 main.py' \
         + ' --max_epoch=' + str(max_epoch) \
