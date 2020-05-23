f1c2fc4338422b1f9f8e461704b1a4ad062ab386
Gursharan Ahir <ahir.sharan@gmail.com>
2020-05-23 19:36:42 +0530

    Update run_meta.py

--- run_meta.py
+++ run_meta.py
@@ -22,7 +22,7 @@ def run_exp(num_batch=50, shot=3, teshot=1, query=1, lr1=0.0005, lr2=0.005, base
         + ' --update_step=' + str(update_step) \
         + ' --way=' + str(way) 
 
-    os.system(the_command + ' --phase=meta_train')
+    #os.system(the_command + ' --phase=meta_train')
     os.system(the_command + ' --phase=meta_eval')
 
 run_exp(num_batch=50, shot=3, teshot=1, query=1, lr1=0.0005, lr2=0.005, base_lr=0.01, update_step=20, gamma=0.5)
