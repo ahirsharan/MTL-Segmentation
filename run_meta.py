2ec0c046cddd06b2eb6a767d9bd2146b8cd55341
Gursharan Ahir <ahir.sharan@gmail.com>
2020-06-06 00:14:12 +0530

    Update run_meta.py

--- run_meta.py
+++ run_meta.py
@@ -6,7 +6,7 @@ import os
 def run_exp(num_batch=50, shot=3, teshot=1, query=1, lr1=0.0005, lr2=0.005, base_lr=0.01, update_step=20, gamma=0.5):
     max_epoch = 200
     step_size = 20
-    way=2
+    way=2 #Backround as a class included. Adjust accordingly.
     gpu=1
        
     the_command = 'python3 main.py' \
